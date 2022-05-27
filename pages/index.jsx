56f038788b3db822eca48b558ab593b40ef1089c
guymeiri <guymeiri@microsoft.com>
2022-05-27 10:31:06 +0300

    added links

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <li>
+        <Link href="/portfolio">Protfolio</Link>
+      </li>
+      <li>
+        <Link href="/clients">clients</Link>
+      </li>
     </div>
   );
 }
